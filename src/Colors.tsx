4d92cd323d79406d59c8eb3886aa2148c895800f
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-01-25 17:49:15 +0100

    Minor cleanup

--- src/Colors.tsx
+++ src/Colors.tsx
@@ -2,7 +2,12 @@ import Color from './Color.tsx'
 import './Colors.css'
 
 function Colors(props: {number: string}) {
-    const colors: JSX.Element[] = Array.from({ length: parseInt(props.number) }, (_, index) => <Color key={index} />);
+    const colors: JSX.Element[] = [];
+
+    for (let i = 0; i < parseInt(props.number); i++) {
+        colors[i] = <Color key={i} />;
+    }
+    
     return (
         <div className="colors-container">
             {colors}
