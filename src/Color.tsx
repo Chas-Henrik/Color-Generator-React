257b7c51bb9779241c74ea642f830cd97ed0d0db
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-02-01 19:32:50 +0100

    Added 'favorite colors' feature

--- src/Color.tsx
+++ src/Color.tsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react'
 import './Color.css'
 let id = 0;
 
 function Color(props: {color: string}) {
+    const [hasBorder, setHasBorder] = useState<boolean>(false);
+    const toggleBorder = () => {
+        setHasBorder(!hasBorder);
+    }
+    const styleObject = {
+        backgroundColor: props.color,
+        border: hasBorder ? '2px solid white' : 'none'
+    }
+
     return (
-        <div className='color' key={id++} style={{backgroundColor: props.color}}>{props.color}</div>
+        <div className='color' key={id++} onClick={toggleBorder} style={styleObject}>{props.color}</div>
         )
 }
 
--- src/Colors.tsx
+++ src/Colors.tsx
@@ -1,19 +1,27 @@
+import { useState } from 'react'
 import Color from './Color.tsx'
 import './Colors.css'
 
 function Colors(props: {number: string}) {
     const colors: JSX.Element[] = [];
+    const [hasColorMap, setColorMap] = useState<boolean>(false);
+    const buttonText = hasColorMap ? 'Remove Colormap' : 'Generate Colormap';
 
-    for (let i = 0; i < parseInt(props.number); i++) {
-        const color:string = '#' + String(Math.floor(16777216*Math.random()).toString(16)).padStart(6, '0');
-        colors[i] = <Color key={i} color={color}/>;
+    if(hasColorMap) {
+        for (let i = 0; i < parseInt(props.number); i++) {
+            const color:string = '#' + String(Math.floor(16777216*Math.random()).toString(16)).padStart(6, '0');
+            colors.push(<Color key={i} color={color}/>);
+        }
     }
 
     return (
-        <div className="colors-container">
-            {colors}
-        </div>
-        )
+        <>
+            <button className="colors-button" onClick={() => setColorMap(!hasColorMap)}>{buttonText}</button>
+            <div className="colors-container">
+                {colors}
+            </div>
+        </>
+    )
 }
 
 export default Colors
